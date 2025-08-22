cf0c6bb75b7555fae55941517636e5e919f42b12
Justin Bruinsma <justinabruinsma@gmail.com>
2025-08-22 19:09:15 -0400

    Refactor ChatDatabase and UserDatabase to inherit directly from AVLTree

--- backend/models/databases/user_database.py
+++ backend/models/databases/user_database.py
@@ -1,21 +1,21 @@
 from backend.models.databases.avl_tree.avl_tree import AVLTree
 
 
-class UserDatabase:
+class UserDatabase(AVLTree):
 
     def __init__(self, file_path):
-        self.users = AVLTree(file_path)
+        super().__init__(file_path)
 
     def add_user(self, username, password):
-        if self.users.search(username) is not None:
+        if self.search(username) is not None:
             raise ValueError("Username already exists.")
-        self.users.insert(username, password)
+        self.insert(username, password)
 
     def search_for_user(self, username):
-        return self.users.search(username)
+        return self.search(username)
 
     def update_username_key(self, old_username, new_username):
-        self.users.change_key(old_username, new_username)
+        self.change_key(old_username, new_username)
 
     def save(self):
-        self.users.save()
+        self.save()
